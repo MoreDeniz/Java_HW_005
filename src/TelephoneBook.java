3f594afc68855bf9228c159272dfda1652809365
Mayya <msalosina@mail.ru>
2023-02-24 20:05:12 +0700

    TelephoneBook

--- src/TelephoneBook.java
+++ src/TelephoneBook.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TelephoneBook {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TelephoneBook {
+    private Map<String, String> telBook = new HashMap<>();
+    void add(String telephone, String name) {
+        telBook.put(telephone, name);
+    }
+
+    void print(String name) {
+        for (Map.Entry<String, String> entry: telBook.entrySet()) {
+            if (entry.getValue().equals(name)) {
+                System.out.println(entry);
+            }
+        }
+    }
 }

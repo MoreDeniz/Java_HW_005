1dabf9424477a1d62ef6ae23a2118af1ca06a76d
Mayya <msalosina@mail.ru>
2023-02-27 12:36:31 +0700

    TelephoneBook

--- src/TelephoneBook.java
+++ src/TelephoneBook.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TelephoneBook {
     private Map<String, String> telBook = new HashMap<>();
@@ -11,6 +8,7 @@ public class TelephoneBook {
 
     void print(String name) {
         for (Map.Entry<String, String> entry: telBook.entrySet()) {
+            List<String> tel = new LinkedList<>();
             if (entry.getValue().equals(name)) {
                 System.out.println(entry);
             }
